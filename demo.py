f716d772fb691874b0d057e4ddd6a7117ff38a82
Jaya Aditya <jayaadi92.y@gmail.com>
2026-09-15 17:11:22 +0530

    initial code commit demo

---
+++ demo.py
@@ -0,0 +1,12 @@
+# print("Welcome to the Python World!")
+
+# from colorama import Fore
+# print(Fore.RED + "Welcome to the Python World!")
+# print(Fore.GREEN + "This is a demo of colored text in Python.")
+# print(Fore.BLUE + "You can use different colors for your text.")
+
+# from colorama import Fore, Style
+# print(Style.BRIGHT + Fore.RED + "VPro Skills")
+
+# from colorama import Fore, Style, Back
+# print(Fore.RED + Style.BRIGHT+ Back.RED + "VPro Skills Educ Tech!!!!")
