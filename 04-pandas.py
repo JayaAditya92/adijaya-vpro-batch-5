45af726cc910753734af29078fea5c044131cddf
Jaya Aditya <jayaadi92.y@gmail.com>
2026-09-16 23:57:07 +0530

    code

--- 04-pandas.py
+++ 04-pandas.py
@@ -7,13 +7,13 @@ marks = pd.DataFrame({
     "student_id":[101,102,103,105],
     "marks":[80,90,75,88]
 })
-<<<<<<< HEAD
+
 result = pd.merge(students,marks,on="student_id",how="inner")
 print(result)
 =======
 # result = pd.merge(students,marks,on="student_id",how="inner")
 # print(result)
->>>>>>> a1767660f1f0c031e45602aa0c8803bc645fefde
+
 
 result = pd.merge(students,marks,on="student_id",how="left")
 print(result)
